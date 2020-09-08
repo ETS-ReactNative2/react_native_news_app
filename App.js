f78e39da46162be2e86c35da58bb99531c8a48b0
Royce Chua <royce236@gmail.com>
2020-09-09 07:37:16 +0800

    Add initial UI and API Call

--- App.js
+++ App.js
@@ -1,13 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { SafeAreaView, StyleSheet, View } from 'react-native';
+import { Text, Title } from 'react-native-paper';
+import axios from 'axios';
 
 export default function App() {
+  const axios = require('axios');
+  const [newsArticles, setNewsArticles] = useState([])
+
+  useEffect(() => {
+    axios.get('http://newsapi.org/v2/top-headlines?country=ph&apiKey=2e938b7bf1464889907ef4f0e99a19da')
+    .then(function (response) {
+      // handle success
+      return response.data
+    })
+    .catch(function (error) {
+      // handle error
+      console.log(error);
+    })
+    .then(function (data) {
+      // always executed
+      console.log(data.articles)
+      setNewsArticles(data.articles)
+    });
+    
+  }, [])
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Title>React Native News</Title>
+
+    </SafeAreaView>
   );
 }
 
@@ -16,6 +38,6 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
   },
 });
