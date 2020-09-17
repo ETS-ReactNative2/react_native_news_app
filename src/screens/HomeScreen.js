d200ef349757f753053d1896d9d4d1f914a6073b
Royce Chua <royce236@gmail.com>
2020-09-18 00:29:06 +0800

    Abstract the API Logic from the Component

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,12 +1,14 @@
 import React, { useState, useEffect } from 'react';
-import { View, StyleSheet, RefreshControl, ImageBackground, Image } from 'react-native';
+import { View, StyleSheet, RefreshControl, ImageBackground, TouchableOpacity } from 'react-native';
 import { Text, ActivityIndicator, Title } from 'react-native-paper';
 import { StatusBar } from 'expo-status-bar';
-import axios from 'axios';
+
 import NewsList from '../components/NewsList';
 
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { FlatList } from 'react-native-gesture-handler';
+
+import NEWS_API from '../api/news_api';
 
 const categoryList = [
     {id:'1',category:'Latest'},
@@ -25,32 +27,28 @@ const wait = (timeout) => {
 }
 
 const HomeScreen = ({ navigation }) => {
-    const axios = require('axios');
-    const [newsArticles, setNewsArticles] = useState([])
+    const [newsArticles, setNewsArticles] = useState([]);
+    const [currentCategory, setCurrentCategory] = useState(categoryList[0].category)
 
     const [refreshing, setRefreshing] = React.useState(false);
 
-    const onRefresh = React.useCallback(() => {
-        setRefreshing(true);
-        fetchNews();
+    const onRefresh = React.useCallback((current_category) => {
+        fetchNews({ category: current_category });
     }, []);
 
-    const fetchNews = () => {
-        axios.get('http://newsapi.org/v2/top-headlines?country=ph&apiKey=2e938b7bf1464889907ef4f0e99a19da')
-        .then(function (response) {
-            // handle success
-            return response.data
-        })
-        .catch(function (error) {
-            // handle error
-            console.log(error);
-        })
-        .then(function (data) {
-            // always executed
-            console.log(data.articles)
-            setNewsArticles(data.articles)
-            setRefreshing(false)
-        });
+    const fetchNews = async ({
+        endpoint='top-headlines', 
+        language='en',   
+        country='ph', 
+        category='general'
+        } = {}) => {
+        setRefreshing(true);
+
+        let requestURL = `${endpoint}?language=${language}&country=${country}&category=${category}`;
+        
+        let data = await NEWS_API.getNews(requestURL)
+        setNewsArticles(data.articles)
+        setRefreshing(false)
     }
   
     useEffect(() => {
@@ -73,9 +71,21 @@ const HomeScreen = ({ navigation }) => {
                             keyExtractor={(item) => item.id}
                             renderItem={({ item, index }) => {
                                 return (
-                                    <View style={{ backgroundColor: '#0390fc', flex:1, borderRadius: 20, marginRight: 10, padding: 10 }}>
+                                    <TouchableOpacity 
+                                        style={{ backgroundColor: '#0390fc', flex:1, borderRadius: 20, marginRight: 10, padding: 10 }}
+                                        onPress={() => {
+                                            if(item.category === 'Latest') {
+                                                setCurrentCategory('')
+                                                fetchNews()
+                                            }
+                                            else {
+                                                setCurrentCategory(item.category.toLowerCase())
+                                                fetchNews({category:item.category.toLowerCase()});
+                                            }
+                                        }}
+                                    >
                                         <Text style={{color:'white', fontWeight:'bold'}}>{item.category}</Text>
-                                    </View>
+                                    </TouchableOpacity>
                                 );
                             }}
                         />
@@ -83,7 +93,7 @@ const HomeScreen = ({ navigation }) => {
                     <View style={{marginTop: 5, flex:1, alignItems:'center', justifyContent:'center'}}>
                         <NewsList 
                             refreshing={refreshing}
-                            onRefresh={onRefresh}
+                            onRefresh={()=>onRefresh(currentCategory)}
                             // data={[]}
                             data={newsArticles}
                             onNewsPressed={(news) => navigation.navigate('NewsWebview', { news })}
